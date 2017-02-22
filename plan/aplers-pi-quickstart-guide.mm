cbb479cb93773ec378351f25dbe673db133aab46
Romilly Cocking <romilly.cocking@gmail.com>
2017-02-22 10:50:46 +0000

    Initial Commit

---
+++ plan/aplers-pi-quickstart-guide.mm
@@ -0,0 +1,220 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node FOLDED="false" ID="Freemind_Link_1331878192" CREATED="1266488194781" MODIFIED="1485077236287"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Pi harware
+    </p>
+    <p style="text-align: center">
+      for APL
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Chosing and using
+    </p>
+    <p style="text-align: center">
+      a Pi for Dyalog APL
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Plan" POSITION="right" ID="_" CREATED="1266488212530" MODIFIED="1266488546784" COLOR="#373dcb">
+<edge COLOR="#808080"/>
+<node TEXT="Define" ID="ID_1502922682" CREATED="1463654381538" MODIFIED="1463654384999">
+<node TEXT="Goal" ID="Freemind_Link_1242386705" CREATED="1266488217621" MODIFIED="1266488568255" COLOR="#37a3cb">
+<node TEXT="Mine" ID="ID_481547358" CREATED="1463654277447" MODIFIED="1463654280053">
+<node TEXT="satisfy APLers who contact me..." ID="ID_1729879122" CREATED="1484240947457" MODIFIED="1484240968174"/>
+<node TEXT="info product for Dyalog website" ID="ID_506279554" CREATED="1484240968825" MODIFIED="1484240982782"/>
+<node TEXT="promote the book" ID="ID_375103225" CREATED="1484240983504" MODIFIED="1484240990143"/>
+</node>
+<node TEXT="Theirs" ID="ID_1878772439" CREATED="1463654280574" MODIFIED="1463654283907">
+<node TEXT="develop and run Dyalog APL on the Pi" ID="ID_1968713878" CREATED="1484240669328" MODIFIED="1484297017417">
+<node TEXT="what you&apos;ll need" FOLDED="true" ID="ID_808152192" CREATED="1484239783538" MODIFIED="1484297037586">
+<node TEXT="Pi" FOLDED="true" ID="ID_648750232" CREATED="1484297230925" MODIFIED="1484297233699">
+<node TEXT="Pi zero" ID="ID_507560516" CREATED="1484297216837" MODIFIED="1484297219617"/>
+<node TEXT="Pi 3" ID="ID_1468771360" CREATED="1484297208613" MODIFIED="1484297216371"/>
+</node>
+<node TEXT="Other hardware" ID="ID_221514313" CREATED="1484297244469" MODIFIED="1484297248834"/>
+</node>
+<node TEXT="where you can get it" FOLDED="true" ID="ID_135659232" CREATED="1484297038509" MODIFIED="1484297042963">
+<node TEXT="and what it will cost" ID="ID_481899843" CREATED="1484297254406" MODIFIED="1484297259033"/>
+</node>
+<node TEXT="how to use it" FOLDED="true" ID="ID_1824464056" CREATED="1484297043421" MODIFIED="1484297084705">
+<node TEXT="setup/installation" ID="ID_254063850" CREATED="1484297055541" MODIFIED="1484297274979"/>
+<node TEXT="later use" FOLDED="true" ID="ID_1811794559" CREATED="1484297064285" MODIFIED="1484297100945">
+<node TEXT="TTY mode" ID="ID_554249871" CREATED="1484297114942" MODIFIED="1484297118233"/>
+<node TEXT="RIDE" FOLDED="true" ID="ID_1984072498" CREATED="1484297118701" MODIFIED="1484297125339">
+<node TEXT="from another networked machine" ID="ID_303370704" CREATED="1484297125957" MODIFIED="1484297147129"/>
+<node TEXT="using the PI" ID="ID_268100073" CREATED="1484297136997" MODIFIED="1484297144143"/>
+</node>
+<node TEXT="non-APL access" FOLDED="true" ID="ID_885483213" CREATED="1484297150773" MODIFIED="1484297155649">
+<node TEXT="via screen, kb" ID="ID_104689825" CREATED="1484297157781" MODIFIED="1484297164441"/>
+<node TEXT="via SSH" ID="ID_853624069" CREATED="1484297165086" MODIFIED="1484297168681"/>
+<node TEXT="via VNC" ID="ID_497571813" CREATED="1484297169189" MODIFIED="1484297174642"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Audience" ID="ID_901216520" CREATED="1463654295853" MODIFIED="1463654300762">
+<node TEXT="APLers new to the PI" ID="ID_184214214" CREATED="1484239795032" MODIFIED="1484239799375"/>
+<node TEXT="Pi users new to APL" FOLDED="true" ID="ID_754382553" CREATED="1484239800384" MODIFIED="1484296951072">
+<icon BUILTIN="button_cancel"/>
+<node TEXT="point at the book" ID="ID_1689504372" CREATED="1484296964341" MODIFIED="1484296970497"/>
+<node TEXT="may be useful if you don&apos;t yet have a Pi" ID="ID_207199621" CREATED="1484297303653" MODIFIED="1484297331706"/>
+</node>
+</node>
+<node TEXT="Venue/format" ID="ID_1402117156" CREATED="1463654503149" MODIFIED="1484298105203">
+<node TEXT="Leanpub" ID="ID_809708656" CREATED="1484298106806" MODIFIED="1484298110474"/>
+<node TEXT="Dyalog website" ID="ID_1423302818" CREATED="1484298111021" MODIFIED="1484298127922"/>
+<node TEXT="Amazon" ID="ID_1802212815" CREATED="1484298118446" MODIFIED="1484298122034"/>
+</node>
+<node TEXT="Duration" ID="ID_190219110" CREATED="1463654512877" MODIFIED="1463654516850"/>
+</node>
+<node TEXT="Design" ID="ID_597550361" CREATED="1487235492826" MODIFIED="1487235492828" LINK="aplers-qs-content.mm">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+</node>
+<node TEXT="Develop" ID="ID_1583658497" CREATED="1463654419456" MODIFIED="1463654428341">
+<node TEXT="Visuals" ID="ID_231033792" CREATED="1463654403097" MODIFIED="1463654407950"/>
+<node TEXT="Script" ID="ID_1096455987" CREATED="1463654408505" MODIFIED="1463654412494"/>
+</node>
+<node TEXT="Deliver" ID="ID_855143644" CREATED="1463654485726" MODIFIED="1463654489355">
+<node TEXT="how" ID="ID_752681429" CREATED="1484239850448" MODIFIED="1484239852421">
+<node TEXT="Text document" ID="ID_1633303309" CREATED="1484239853688" MODIFIED="1484239857766"/>
+<node TEXT="Video" ID="ID_1171747105" CREATED="1484239858225" MODIFIED="1484239868926"/>
+</node>
+<node TEXT="Promote" ID="ID_864038486" CREATED="1484298141654" MODIFIED="1484298144611"/>
+<node TEXT="Rehearse" ID="ID_179822292" CREATED="1463654491637" MODIFIED="1463654495074"/>
+<node TEXT="Revise" ID="ID_1015439316" CREATED="1463654496405" MODIFIED="1463654552528"/>
+<node TEXT="Present" ID="ID_1453452925" CREATED="1463654553043" MODIFIED="1463654556953"/>
+<node TEXT="Review" ID="ID_996785710" CREATED="1463654557444" MODIFIED="1463654560432"/>
+</node>
+</node>
+<node TEXT="Tasks" POSITION="right" ID="Freemind_Link_857865306" CREATED="1266488223477" MODIFIED="1266488636684" COLOR="#de5613">
+<node TEXT="move to new project" ID="ID_1308558871" CREATED="1487757095591" MODIFIED="1487757130012">
+<node TEXT="create project" ID="ID_1590760625" CREATED="1487757140959" MODIFIED="1487757147481">
+<node TEXT="share on GitHub" ID="ID_1015648402" CREATED="1487757187391" MODIFIED="1487757193857">
+<node TEXT="CC/SA" ID="ID_724106882" CREATED="1487757196391" MODIFIED="1487757203711"/>
+</node>
+</node>
+<node TEXT="move mind map" ID="ID_350588676" CREATED="1487757148527" MODIFIED="1487757156066"/>
+<node TEXT="get fm2md working locally" ID="ID_1883629986" CREATED="1487757164183" MODIFIED="1487757176445"/>
+<node TEXT="get pandoc working locall" ID="ID_664452871" CREATED="1487757177223" MODIFIED="1487757184225"/>
+</node>
+<node TEXT="Pre-announce and do survey/competition about topics" ID="ID_1997973893" CREATED="1484298191518" MODIFIED="1484298289930"/>
+<node TEXT="set up slack channel" ID="ID_1141734921" CREATED="1487235606978" MODIFIED="1487235615647"/>
+<node TEXT="pictures" ID="ID_1559091989" CREATED="1487236995418" MODIFIED="1487237019949">
+<node TEXT="Pi 3" ID="ID_1527072823" CREATED="1487237057330" MODIFIED="1487237060288"/>
+<node TEXT="SD card" ID="ID_1890052620" CREATED="1487237060746" MODIFIED="1487237064161"/>
+<node TEXT="Pi zero" ID="ID_1552255852" CREATED="1487237065290" MODIFIED="1487237068632"/>
+<node TEXT="pimoroni Hub" ID="ID_1761896259" CREATED="1487237069042" MODIFIED="1487237079068"/>
+</node>
+<node TEXT="tools" ID="ID_1525222040" CREATED="1487756938902" MODIFIED="1487756949707">
+<node TEXT="fm2md" ID="ID_541892737" CREATED="1487756950510" MODIFIED="1487756953868"/>
+<node TEXT="pandoc" ID="ID_439644161" CREATED="1487756954286" MODIFIED="1487756957756"/>
+</node>
+</node>
+<node TEXT="Review" POSITION="left" ID="Freemind_Link_1153466861" CREATED="1266488259116" MODIFIED="1266488895851" COLOR="#fb1802">
+<icon BUILTIN="xmag"/>
+<node TEXT="WW" ID="Freemind_Link_1466796877" CREATED="1266488965666" MODIFIED="1296303686570" COLOR="#ffcc00" BACKGROUND_COLOR="#006666">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="WDNW" ID="Freemind_Link_1910608691" CREATED="1266488980018" MODIFIED="1296303689746">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node TEXT="WDD" ID="Freemind_Link_1294877740" CREATED="1266488984697" MODIFIED="1296303577864" COLOR="#00cc00">
+<icon BUILTIN="licq"/>
+</node>
+<node TEXT="Questions" ID="Freemind_Link_1003502265" CREATED="1266489024048" MODIFIED="1296303570801" COLOR="#00cccc">
+<icon BUILTIN="help"/>
+</node>
+<node TEXT="Surprises" ID="Freemind_Link_19217072" CREATED="1266489032944" MODIFIED="1296303119998" COLOR="#ff0000" BACKGROUND_COLOR="#ffff00">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node TEXT="Feelings" ID="Freemind_Link_1706517660" CREATED="1266489043119" MODIFIED="1296303692921" COLOR="#cc0000">
+<icon BUILTIN="ksmiletris"/>
+</node>
+<node TEXT="Ideas" ID="Freemind_Link_1517433488" CREATED="1266488273147" MODIFIED="1266488921179" COLOR="#36d12a">
+<icon BUILTIN="idea"/>
+</node>
+<node TEXT="Fridge" ID="Freemind_Link_1954142386" CREATED="1266488793745" MODIFIED="1266488959040" COLOR="#9679df">
+<icon BUILTIN="desktop_new"/>
+</node>
+</node>
+<node TEXT="Record" POSITION="left" ID="Freemind_Link_813012694" CREATED="1266488248876" MODIFIED="1266488728945" COLOR="#27444b">
+<icon BUILTIN="pencil"/>
+</node>
+<node TEXT="Resources" POSITION="left" ID="Freemind_Link_1594441695" CREATED="1273319832507" MODIFIED="1296303609303" COLOR="#33ff33">
+<icon BUILTIN="password"/>
+</node>
+</node>
+</map>
---
+++ plan/aplers-qs-content.mm
@@ -0,0 +1,226 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Design" FOLDED="false" ID="ID_352043439" CREATED="1463654329188" MODIFIED="1487235492757" LINK="aplers-pi-quickstart-guide.mm">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Introduction" POSITION="right" ID="ID_397673433" CREATED="1463654308349" MODIFIED="1487237569561">
+<node TEXT="The Raspberry Pi is cheap to buy, fun to explore, and a very practical platform for APL. If you know APL, this book will help you to get going with the Pi as quickly as posible." ID="ID_165494090" CREATED="1484301928167" MODIFIED="1484302256315"/>
+</node>
+<node TEXT="Topics" POSITION="right" ID="ID_68788707" CREATED="1463654311204" MODIFIED="1463654314234">
+<node TEXT="Title" ID="ID_692804023" CREATED="1484298331526" MODIFIED="1484298337018">
+<node TEXT="The APLer&apos;s Quickstart Guide to the Raspberry Pi" ID="ID_198048181" CREATED="1484298338022" MODIFIED="1484298556892"/>
+<node TEXT="What to buy, where to get it, how to set it up" ID="ID_1898818106" CREATED="1484298407094" MODIFIED="1484301851273"/>
+</node>
+<node TEXT="executive summary :)" ID="ID_1078817923" CREATED="1487237177762" MODIFIED="1487237546601"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This section contains the bare minimum of advice you need to get started quickly, but offers no explanation or discussion of alternative approaches.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      If you're happy with that just do it :)
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Most APLers don't just blindly follow the herd - that's why they are APLers. If you want to know the assumptions behind this advice and the alternatives available, read the whole document. It's quite short.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Buy Pi 3 starter kit" ID="ID_127457421" CREATED="1487237195882" MODIFIED="1487237211718">
+<node TEXT="contents" ID="ID_1541578998" CREATED="1487756876718" MODIFIED="1487756880739"/>
+</node>
+<node TEXT="Connect like this" ID="ID_107567104" CREATED="1487237225650" MODIFIED="1487237232883"/>
+<node TEXT="On first boot chose raspbian" ID="ID_1871729471" CREATED="1487237212682" MODIFIED="1487237223684"/>
+<node TEXT="Install Dyalog following these instructions" ID="ID_746725502" CREATED="1487237242698" MODIFIED="1487237273380"/>
+</node>
+<node TEXT="How much will it cost to get started?" ID="ID_991331726" CREATED="1484549494671" MODIFIED="1487237584833">
+<node TEXT="Probably about &#xa3;30-&#xa3;50" ID="ID_156007856" CREATED="1484553220457" MODIFIED="1484553372998"/>
+<node TEXT="Max &#xa3;250" ID="ID_984469032" CREATED="1484553164705" MODIFIED="1487262467127">
+<node TEXT="Everything including new monitor" ID="ID_37495176" CREATED="1487237606898" MODIFIED="1487237634115"/>
+<node TEXT="Unbox" ID="ID_629006054" CREATED="1484553205929" MODIFIED="1484553210454"/>
+<node TEXT="Set up" ID="ID_1006328720" CREATED="1484553211673" MODIFIED="1484553214533"/>
+<node TEXT="Connect" ID="ID_1023699038" CREATED="1484553214961" MODIFIED="1484553217526"/>
+</node>
+<node TEXT="Min &#xa3;4 + Postage" ID="ID_1173795760" CREATED="1484553172089" MODIFIED="1484553287661">
+<node TEXT="spend a day or two reserching fiddling with software" ID="ID_1908643344" CREATED="1484553237889" MODIFIED="1487237660291">
+<node TEXT="may get stuck" ID="ID_661974202" CREATED="1487237638138" MODIFIED="1487237642441"/>
+</node>
+<node TEXT="use a laptop or desktop computer" ID="ID_1567637503" CREATED="1484553254569" MODIFIED="1484553267845"/>
+<node TEXT="reuse an existing SD card" ID="ID_337001851" CREATED="1487236784306" MODIFIED="1487236824347"/>
+</node>
+</node>
+<node TEXT="What&apos;s not covered" ID="ID_164075736" CREATED="1484328074736" MODIFIED="1484328080173">
+<node TEXT="APL" ID="ID_421185591" CREATED="1484549276991" MODIFIED="1484549279204"/>
+<node TEXT="Linux" ID="ID_200430936" CREATED="1484549279623" MODIFIED="1484549286356"/>
+<node TEXT="Physical computing" FOLDED="true" ID="ID_1465979509" CREATED="1484549294079" MODIFIED="1484549312190">
+<node TEXT="GPIO" ID="ID_1860539798" CREATED="1484549312815" MODIFIED="1484549316172"/>
+</node>
+</node>
+<node TEXT="Audience" ID="ID_159752883" CREATED="1484328106769" MODIFIED="1484328114325">
+<node TEXT="Familiar with APL" ID="ID_103961847" CREATED="1484328041185" MODIFIED="1484328049271"/>
+<node TEXT="Less familiar (or a novice) with the Pi" ID="ID_1249888207" CREATED="1484328049825" MODIFIED="1484328197173"/>
+</node>
+<node TEXT="Assumptions" ID="ID_958143184" CREATED="1484327949945" MODIFIED="1484327954350">
+<node TEXT="Simplest setup rather than lowest cost" FOLDED="true" ID="ID_1804990019" CREATED="1484327553752" MODIFIED="1484327582493">
+<node TEXT="Cost saving alternatives in appendix A." FOLDED="true" ID="ID_1522864436" CREATED="1484327586776" MODIFIED="1484327650317">
+<node TEXT="Headless working" ID="ID_1851147570" CREATED="1484549180519" MODIFIED="1484549214731"/>
+</node>
+</node>
+<node TEXT="Using latest (PIXEL) version of the Rapberry Pi OS" ID="ID_1816516421" CREATED="1484327998144" MODIFIED="1484328019990"/>
+<node TEXT="Existing monitor" ID="ID_1852618697" CREATED="1484549505551" MODIFIED="1484549510828"/>
+</node>
+<node TEXT="What to buy" ID="ID_876324595" CREATED="1484327358856" MODIFIED="1484327363469">
+<node FOLDED="true" ID="ID_1419025674" CREATED="1484327385225" MODIFIED="1484328154369"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Pi
+    </p>
+  </body>
+</html>
+</richcontent>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pros/cons
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Raspberry Pi 3" ID="ID_1480988330" CREATED="1484327366176" MODIFIED="1484327372181"/>
+<node TEXT="Raspberry Pi zero" ID="ID_1335676460" CREATED="1484327372777" MODIFIED="1484327379341"/>
+</node>
+<node TEXT="with Pi 3" FOLDED="true" ID="ID_1903900261" CREATED="1484327393848" MODIFIED="1484327430581">
+<node TEXT="Power supply (2.5 A)" ID="ID_167083369" CREATED="1484327431673" MODIFIED="1484327440333"/>
+<node TEXT="HDMI cable to connect to monitor" ID="ID_151681110" CREATED="1484327441088" MODIFIED="1484327460222"/>
+<node TEXT="USB keyboard" ID="ID_1231074086" CREATED="1484327461297" MODIFIED="1484327496741"/>
+<node TEXT="Mouse" ID="ID_1898286309" CREATED="1484327497297" MODIFIED="1484327503526"/>
+<node TEXT="assume you have Wifi network and/or ethernet cable" ID="ID_877979609" CREATED="1484327509057" MODIFIED="1484327984357"/>
+<node TEXT="SD card" FOLDED="true" ID="ID_488467305" CREATED="1484327527281" MODIFIED="1484327531630">
+<node TEXT="NOOBS or Raspbian" ID="ID_1026698786" CREATED="1484327533225" MODIFIED="1484327546726"/>
+</node>
+</node>
+<node TEXT="with Pi zero" FOLDED="true" ID="ID_793630936" CREATED="1484327667081" MODIFIED="1484327671485">
+<node TEXT="USB hub" ID="ID_1209393694" CREATED="1484327673064" MODIFIED="1484327676549"/>
+<node TEXT="Power supply (2 A)" ID="ID_648921794" CREATED="1484327679801" MODIFIED="1484327689038"/>
+<node TEXT="HDMI cable and adaptor" ID="ID_1220653965" CREATED="1484327689737" MODIFIED="1484327702207"/>
+<node TEXT="USB keyboard" ID="ID_1291864507" CREATED="1484327702881" MODIFIED="1484327716542"/>
+<node TEXT="Mouse" ID="ID_1546489304" CREATED="1484327717145" MODIFIED="1484327720109"/>
+</node>
+<node TEXT="Pi compute module" ID="ID_57021806" CREATED="1487236840778" MODIFIED="1487236856627">
+<icon BUILTIN="closed"/>
+</node>
+</node>
+<node TEXT="Where to get it" FOLDED="true" ID="ID_1367483236" CREATED="1484327618848" MODIFIED="1484327635582">
+<node TEXT="Pi zero" ID="ID_654932136" CREATED="1484328125345" MODIFIED="1484328131780"/>
+</node>
+<node TEXT="Setup" ID="ID_1869458108" CREATED="1484327724905" MODIFIED="1484327728950">
+<node TEXT="(Flashing an SD card - see appendix A)" ID="ID_1753163702" CREATED="1484327898209" MODIFIED="1484327925564"/>
+<node TEXT="First boot" ID="ID_223962810" CREATED="1484327883225" MODIFIED="1484327890781"/>
+<node TEXT="Update/Upgrade" ID="ID_812972878" CREATED="1484327933497" MODIFIED="1484327943637"/>
+</node>
+<node TEXT="Subsequent use" ID="ID_1920101404" CREATED="1484327729553" MODIFIED="1484327736789">
+<node TEXT="APL" ID="ID_1315148727" CREATED="1484327788417" MODIFIED="1484327790022">
+<node TEXT="RIDE" ID="ID_804540360" CREATED="1484327738481" MODIFIED="1484327741693">
+<node TEXT="On Laptop/desktop" ID="ID_666659479" CREATED="1484327768521" MODIFIED="1484327785677"/>
+<node TEXT="On Pi" ID="ID_296080013" CREATED="1484327804393" MODIFIED="1484327809231"/>
+</node>
+<node TEXT="TTY mode" ID="ID_963265110" CREATED="1484327810777" MODIFIED="1487758917829">
+<icon BUILTIN="closed"/>
+</node>
+</node>
+<node TEXT="Linux" ID="ID_1028780444" CREATED="1484327790681" MODIFIED="1484327794622">
+<node TEXT="Pi mouse, keyboard, screen" ID="ID_249177229" CREATED="1484327837817" MODIFIED="1484327856429"/>
+<node TEXT="SSH" ID="ID_884725699" CREATED="1484327742168" MODIFIED="1487758930973">
+<icon BUILTIN="closed"/>
+</node>
+<node TEXT="VNC" ID="ID_178278153" CREATED="1484327749417" MODIFIED="1484327753589">
+<node TEXT="VNC connect" ID="ID_1869860204" CREATED="1487758935670" MODIFIED="1487758942556"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="End" POSITION="right" ID="ID_148697897" CREATED="1463654305117" MODIFIED="1463654307691">
+<node TEXT="Other resources" ID="ID_1514081994" CREATED="1487236895010" MODIFIED="1487236906132">
+<node TEXT="slack channel" ID="ID_321155598" CREATED="1487236907370" MODIFIED="1487236914757"/>
+<node TEXT="email list" ID="ID_1187967715" CREATED="1487236915930" MODIFIED="1487236938500"/>
+</node>
+<node TEXT="I hope you&apos;ll have as much fun with the Pi as I do." ID="ID_394088020" CREATED="1484301882039" MODIFIED="1487236966872"/>
+<node TEXT="Physical computing" ID="ID_991846512" CREATED="1484301891463" MODIFIED="1484301903395">
+<node TEXT="Course" ID="ID_445187392" CREATED="1487758959510" MODIFIED="1487758975492"/>
+</node>
+</node>
+</node>
+</map>
